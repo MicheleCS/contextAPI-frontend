a396e0e6fa19d57b72f0b5a99409edcb36bf0152
MicheleCS <michelesouza-@outlook.com>
2023-06-01 08:48:38 -0300

    configurando validação do usuário no login da Página Privada

--- src/pages/Private/index.tsx
+++ src/pages/Private/index.tsx
@@ -1,5 +1,12 @@
+import { useContext } from "react"
+import { AuthContext } from "../../contexts/Auth/AuthContexts"
+
 export const Private = () => {
+  const auth = useContext(AuthContext);
   return (
-    <div>Página Privada</div>
+    <div>
+      <h1>Private Page</h1>
+      Olá {auth.user?.name}
+    </div>
   )
 }
